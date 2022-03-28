386664e8b28bb96bacd568b270128256ccb58b62
JotaroKujo0525 <77835007+JotaroKujo0525@users.noreply.github.com>
2022-03-28 12:39:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ bot.readyCommand({
 
 bot.command({
     name: 'help',
-    code: `Hello! This bot will be temporarily be **SHUTDOWN**, but this doesn't mean it's permanent. I just can't handle the pressure of my two bots because i'm the only one maintaining it. This bot will be continued after my study. You can join our [support server](https://discord.gg/cgjW7Xr2ns) for follow-ups and questions. Thank You for following me on this journey!`
+    code: `$description[Hello! This bot will be temporarily be **SHUTDOWN**, but this doesn't mean it's permanent. I just can't handle the pressure of my two bots because i'm the only one maintaining it. This bot will be continued after my study. You can join our [support server](https://discord.gg/cgjW7Xr2ns) for follow-ups and questions. Thank You for following me on this journey!]`
 })
 
